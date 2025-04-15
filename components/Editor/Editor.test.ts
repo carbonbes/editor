a5824218a4742e54efbed63a59a70bc56d52321a
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-04-15 21:51:26 +0300

    feat/editor component

--- components/Editor/Editor.test.ts
+++ components/Editor/Editor.test.ts
@@ -1,16 +1,33 @@
 import { describe, expect, test } from 'vitest'
 import { flushPromises, mount } from '@vue/test-utils'
 import Editor from '~/components/Editor/Editor.client.vue'
+import type { Content } from '@tiptap/vue-3'
 
 describe('Editor', () => {
+  async function mountEditor(content?: Content) {
+    const wrapper = mount(Editor, { props: { content } })
+
+    await flushPromises()
+
+    return wrapper
+  }
+
   test('should be defined', () => {
     expect(Editor).toBeDefined()
   })
 
-  test('renders the correct markup without content', () => {
+  test('renders the correct markup without content', async () => {
     const wrapper = mount(Editor)
 
-    expect(wrapper.element.innerHTML).toBe('')
+    await flushPromises()
+
+    expect(wrapper.element.innerHTML).toBe(
+      '<div contenteditable="true" role="textbox" translate="no" class="tiptap ProseMirror" tabindex="0">' +
+        '<p><br class="ProseMirror-trailingBreak"></p>' +
+      '</div>'
+    )
+
+    wrapper.unmount()
   })
 
   test('renders the correct markup with content', async () => {
@@ -27,5 +44,7 @@ describe('Editor', () => {
         '<h1>Всем привет!</h1>' +
       '</div>'
     )
+
+    wrapper.unmount()
   })
 })

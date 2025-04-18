e4cc994eee36127fd65b835fe1027d1a073ab6d9
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-04-18 12:32:37 +0300

    fix/clear unused plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
 
@@ -12,5 +11,5 @@ export default defineNuxtConfig({
     plugins: [tailwindcss()],
   },
 
-  modules: ['@nuxt/test-utils/module', '@nuxt/eslint', '@vueuse/nuxt'],
+  modules: ['@nuxt/test-utils/module', '@nuxt/eslint'],
 })

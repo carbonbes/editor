9818fed651aa0914bf22bf37997f6a99a926f471
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-05-13 12:44:11 +0300

    fix/transition between

--- components/Shared/Transitions/TransitionBetween.vue
+++ components/Shared/Transitions/TransitionBetween.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="relative" :style="`--transition-between-duration: ${duration}ms`">
     <Transition
-      enter-active-class="absolute transition-[translate,opacity] duration-(--transition-between-duration) ease-in-out"
+      :enter-active-class="enterLeaveActiveClass"
+      :leave-active-class="enterLeaveActiveClass"
+      :enter-to-class="enterToLeaveFromClass"
+      :leave-from-class="enterToLeaveFromClass"
       :enter-from-class
-      enter-to-class="translate-x-0 opacity-100"
-      leave-active-class="absolute transition-[translate,opacity] duration-(--transition-between-duration) ease-in-out"
-      leave-from-class="translate-x-0 opacity-100"
       :leave-to-class
-      @before-enter="emits('beforeEnter')"
-      @enter="emits('enter')"
-      @after-enter="emits('afterEnter')"
-      @enter-cancelled="emits('enterCancelled')"
-      @before-leave="emits('beforeLeave')"
-      @leave="emits('leave')"
-      @after-leave="emits('afterLeave')"
-      @leave-cancelled="emits('leaveCancelled')"
     >
       <slot />
     </Transition>
@@ -22,38 +14,36 @@
 </template>
 
 <script setup lang="ts">
-const { duration = 250 } = defineProps<{
+export interface TransitionBetweenProps {
+  index: number
   duration?: number
-}>()
+}
 
-const emits = defineEmits([
-  'beforeEnter',
-  'enter',
-  'afterEnter',
-  'enterCancelled',
-  'beforeLeave',
-  'leave',
-  'afterLeave',
-  'leaveCancelled',
-])
-
-const index = defineModel<number>('index', { default: 0 })
+const { index, duration = 250 } = defineProps<TransitionBetweenProps>()
 
 const direction = ref<-1 | 1>()
 
 watch(
-  index,
+  () => index,
   (newIndex, oldIndex) => {
-    if (newIndex > oldIndex) direction.value = 1
-    else direction.value = -1
+    direction.value = newIndex > oldIndex ? 1 : -1
   },
 )
 
+const enterLeaveActiveClass =
+  'absolute transition-[translate,opacity] duration-(--transition-between-duration) ease-in-out'
+
+const enterToLeaveFromClass = 'translate-x-0 opacity-100'
+
 const enterFromClass = computed(() =>
-  direction.value === 1 ? 'translate-x-full opacity-0' : '-translate-x-full opacity-0',
+  direction.value === 1
+    ? 'translate-x-full opacity-0'
+    : '-translate-x-full opacity-0',
 )
 
 const leaveToClass = computed(() =>
-  direction.value === 1 ? '-translate-x-full opacity-0' : 'translate-x-full opacity-0',
+  direction.value === 1
+    ? '-translate-x-full opacity-0'
+    : 'translate-x-full opacity-0',
 )
 </script>

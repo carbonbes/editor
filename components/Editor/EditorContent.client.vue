82622be36b17eca8c0296038d2ac76905db4e3c4
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-05-22 12:56:45 +0300

    fix/editor content

--- components/Editor/EditorContent.client.vue
+++ components/Editor/EditorContent.client.vue
@@ -1,10 +1,9 @@
 <template>
-  <EditorContent :editor class="h-full" :class="{ 'overflow-x-hidden': isTouch }" />
+  <EditorContent :editor class="h-full" />
 </template>
 
 <script lang="ts" setup>
 import { EditorContent } from '@tiptap/vue-3'
 
 const { editor } = useEditor()
-const { isTouch } = useDevice()
 </script>

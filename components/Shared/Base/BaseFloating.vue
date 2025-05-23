61d3d3b672f1a794c1bf6d5650855cdafc227a13
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-05-23 22:36:45 +0300

    feat/base overlay

---
+++ components/Shared/Base/BaseFloating.vue
@@ -0,0 +1,15 @@
+<template>
+  <Primitive
+    v-bind="forwarded"
+    class="bg-white ring-1 ring-gray-200 shadow-lg"
+  >
+    <slot />
+  </Primitive>
+</template>
+
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps, useForwardProps } from 'reka-ui'
+
+const props = defineProps<PrimitiveProps>()
+const forwarded = useForwardProps(props)
+</script>
---
+++ components/Shared/Base/BaseOverlay.vue
@@ -0,0 +1,15 @@
+<template>
+  <Primitive
+    v-bind="forwarded"
+    class="fixed inset-0 bg-black/25 backdrop-blur-[2px]"
+  >
+    <slot />
+  </Primitive>
+</template>
+
+<script lang="ts" setup>
+import { Primitive, useForwardProps, type PrimitiveProps } from 'reka-ui'
+
+const props = defineProps<PrimitiveProps>()
+const forwarded = useForwardProps(props)
+</script>

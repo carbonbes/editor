c726fd34a1d832eed58f015aea4c251de5f0931c
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-06-30 14:16:43 +0300

    fix/video

---
+++ components/Shared/Video.vue
@@ -0,0 +1,18 @@
+<template>
+  <video v-bind="forwarded" />
+</template>
+
+<script setup lang="ts">
+import { useForwardProps } from 'reka-ui'
+
+export interface VideoProps {
+  src: string
+  controls?: boolean
+  muted?: boolean
+  playsInline?: boolean
+  autoPlay?: boolean
+}
+
+const props = defineProps<VideoProps>()
+const forwarded = useForwardProps(props)
+</script>
---
+++ components/Shared/VideoThumbnail.vue
@@ -0,0 +1,9 @@
+<template>
+  <Image :src="thumbnail" />
+</template>
+
+<script setup lang="ts">
+import Image from '~/components/Shared/Image.vue'
+
+const { thumbnail } = defineProps<{ thumbnail: string }>()
+</script>
